b456e9db09f7276520fab865f3abb00e9ac87732
Alonso <jorgealonsovasquez@hotmail.com>
2025-09-29 16:40:35 -0600

    Add Gazebo simulation support and system tests for autonomous robot
    - Created `autonomous_robot_gazebo` package with necessary files including LICENSE, package.xml, setup.py, and test files.
    - Added RViz configuration file for visualizing the robot model and TF frames.
    - Implemented integration tests in `autonomous_robot_system_tests` package with copyright and style checks.
    - Included dependencies for Gazebo and ROS 2 control in the package.xml.
    - Established basic structure for both packages with appropriate setup configurations.

--- build/autonomous_robot/colcon_command_prefix_build.sh
+++ build/autonomous_robot/colcon_command_prefix_build.sh
@@ -1,2 +1,5 @@
 # generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/alonso/Development/School/ros_dev/install/autonomous_robot_bringup/share/autonomous_robot_bringup/package.sh"
 . "/home/alonso/Development/School/ros_dev/install/autonomous_robot_description/share/autonomous_robot_description/package.sh"
+. "/home/alonso/Development/School/ros_dev/install/autonomous_robot_gazebo/share/autonomous_robot_gazebo/package.sh"
+. "/home/alonso/Development/School/ros_dev/install/autonomous_robot_system_tests/share/autonomous_robot_system_tests/package.sh"
---
+++ src/autonomous_robot/autonomous_robot_system_tests/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'autonomous_robot_system_tests'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='alonso',
+    maintainer_email='jorgealonsovasquez@hotmail.com',
+    description='TODO: Package description',
+    license='BSD-3-Clause',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
