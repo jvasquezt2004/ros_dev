c0ee388d85353e148e4360c4c5a2a2ba7ea075f7
Jorge Alonso Vasquez Trujillo <jorgealonsovasquez@hotmail.com>
2025-10-12 17:03:20 -0600

    chore: Stop tracking build, install, and log directories

--- build/autonomous_robot_bringup/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/alonso/Dev/ros_dev/install/autonomous_robot_bringup'
--- build/autonomous_robot_gazebo/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/alonso/Dev/ros_dev/install/autonomous_robot_bringup/share/autonomous_robot_bringup/package.sh"
